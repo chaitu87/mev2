5ce6d634fb6141cbf2a05cb5d80dfc5b577dca4c
Chaithanya Padi <chaithanya@theinnerhour.com>
2017-12-23 07:08:50 +0530

    missing fucks

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,43 +4,51 @@ var auth = require('./auth'),
 require('../data/Models/Article');
 var Article = mongoose.model('Article');
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.get('/register', controllers.users.getRegister);
     app.post('/register', controllers.users.createUser);
     app.post('/login', auth.login);
     app.get('/logout', auth.logout);
     app.get('/login', controllers.users.getLogin);
-    app.get('/', function(req, res) {
-        res.render('index', { currentUser: req.user });
+    app.get('/', function (req, res) {
+        Article.find(function (err, articles) {
+            console.log(articles);
+            if (err) return next(err);
+            res.render('index', {
+                title: 'The Night Captain',
+                articles: articles,
+                currentUser: req.user
+            });
+        });
     });
-    app.get('/projects', function(req, res, next) {
+    app.get('/projects', function (req, res, next) {
         res.render('projects', {
             title: "Projects"
         });
     });
-    app.get('/project/troops', function(req, res, next) {
+    app.get('/project/troops', function (req, res, next) {
         res.render('projects/troops/index', {
             title: "Troops"
         });
     });
-    app.get('/project/troops/preview', function(req, res, next) {
+    app.get('/project/troops/preview', function (req, res, next) {
         res.render('projects/troops/preview', {
             title: "Troops"
         });
     });
-    app.get('/project/web-presentation', function(req, res, next) {
+    app.get('/project/web-presentation', function (req, res, next) {
         res.render('projects/web-presentation/index', {
             title: "Web Presentation"
         });
     });
-    app.get('/snippets', function(req, res, next) {
+    app.get('/snippets', function (req, res, next) {
         res.render('snippets');
     });
-    app.get('/snippet/webworker', function(req, res, next) {
+    app.get('/snippet/webworker', function (req, res, next) {
         res.render('snippets/webworker');
     });
-    app.get('/articles', function(req, res, next) {
-        Article.find(function(err, articles) {
+    app.get('/articles', function (req, res, next) {
+        Article.find(function (err, articles) {
             console.log(articles);
             if (err) return next(err);
             res.render('articles/list', {
@@ -49,10 +57,10 @@ module.exports = function(app) {
             });
         });
     });
-    app.post('/articles', function(req, res, next) {
+    app.post('/articles', function (req, res, next) {
         var payload = req.body;
         console.log(payload);
-        Article.create(payload, function(err) {
+        Article.create(payload, function (err) {
             console.log('created');
             if (err) return next(err);
             res.json({
@@ -60,36 +68,42 @@ module.exports = function(app) {
             });
         });
     });
-    app.post('/article/:id', function(req, res, next) {
+    app.post('/article/:id', function (req, res, next) {
         var payload = req.body;
         console.log(payload);
-        Article.findById(req.params.id,function(err, blog){
+        Article.findById(req.params.id, function (err, blog) {
             blog.title = payload.title;
             blog.body = payload.body;
             blog.shortdesc = payload.shortdesc;
-            blog.save(function(err, updatedArticle){
-                res.render('articles/edit',{article: updatedArticle});
+            blog.save(function (err, updatedArticle) {
+                res.render('articles/edit', {
+                    article: updatedArticle
+                });
             })
         });
     });
-    app.get('/article/:slug', function(req, res, next) {
-        Article.find({
+    app.get('/article/:slug', function (req, res, next) {
+        Article.findOne({
             slug: req.params.slug
-        },function(err, data){
+        }, function (err, data) {
             console.log(data);
-            res.render('article/view',{article: data});
+            res.render('articles/view', {
+                article: data
+            });
         });
     });
-    app.get('/edit/:id', function(req, res, next) {
+    app.get('/edit/:id', function (req, res, next) {
         Article.find({
             _id: req.params.id
-        },function(err, data){
+        }, function (err, data) {
             console.log(data);
-            res.render('articles/edit',{article: data[0]});
+            res.render('articles/edit', {
+                article: data[0]
+            });
         });
     });
-    app.get('/admin', function(req, res, next) {
-        Article.find(function(err, articles) {
+    app.get('/admin', function (req, res, next) {
+        Article.find(function (err, articles) {
             console.log(articles);
             if (err) return next(err);
             res.render('admin/index', {
